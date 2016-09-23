db3b7dbd16681a004843ea068d7291dfca1a4f47
Veelsgaard93 <veelsgaard@gmail.com>
2016-09-23 10:54:39 +0200

    Add project files.

---
+++ SoapTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter celcius degrees");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            // to get serviceRef: rightclick project -> add -> servicereference -> paste wsdl
+
+            ServiceReference1.ConvertTemperatureSoapClient converter = new ServiceReference1.ConvertTemperatureSoapClient("ConvertTemperatureSoap");
+
+            //solve endpoint issue: delete all but one endpoint or specify endpoint in string when instantiated
+            // you find name in the webconfig -> endpoint
+
+            double fahrenheit = converter.ConvertTemp(celsius,
+                ServiceReference1.TemperatureUnit.degreeCelsius,
+                ServiceReference1.TemperatureUnit.degreeFahrenheit);
+            Console.WriteLine("fino bambino - det svarer til: " + fahrenheit);
+
+        }
+    }
+}
